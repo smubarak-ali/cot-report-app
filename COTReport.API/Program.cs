e4b392226382dbdd6b5db35271450bc3eb0efa39
Mubarak Ali <mubarakali.00@gmail.com>
2023-04-10 00:39:57 +0800

    Add the docker file but ignored it in git because of the environment variables.

--- COTReport.API/Program.cs
+++ COTReport.API/Program.cs
@@ -4,17 +4,21 @@ using COTReport.DAL.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("MyLocal", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddDbContext<ReportDbContext>();
 builder.Services.AddScoped<ReportRepository>();
 builder.Services.AddScoped<MyFxbookHelper>();
 
 builder.Services.AddMemoryCache();
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseCors("MyLocal");
 app.MapControllers();
-
 app.Run();
