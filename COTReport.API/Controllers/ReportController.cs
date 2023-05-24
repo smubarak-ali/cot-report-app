3028b9cb1eb3c9cc14d4da91d8382ec9d4fe7c32
Mubarak Ali <mubarakali.00@gmail.com>
2023-05-24 20:24:20 +0800

    Changed the sentiment cache time

--- COTReport.API/Controllers/ReportController.cs
+++ COTReport.API/Controllers/ReportController.cs
@@ -90,7 +90,7 @@ namespace COTReport.API.Controllers
                 if (redisList == null)
                 {
                     var list = await _sentimentRepo.GetSentimentsAsync();
-                    var cacheEntryOptions = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)).SetSlidingExpiration(TimeSpan.FromMinutes(6));
+                    var cacheEntryOptions = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(7)).SetSlidingExpiration(TimeSpan.FromMinutes(5));
                     await _cache.SetStringAsync(cacheKey, JsonConvert.SerializeObject(list), cacheEntryOptions);
                     return Ok(new { Symbols = list });
                 }
